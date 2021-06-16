bd310bb3d2f8d8bc8c61c101179745590de483bf
subject026 <sudobuildspeakers@gmail.com>
2021-06-16 15:06:35 +0100

    boof

--- plugin.php
+++ plugin.php
@@ -3,3 +3,15 @@
 /**
  * Plugin Name: This is big plugin
  */
+
+require_once './vendor/bin/composer/autoload.php';
+
+class Cheese {
+  public function __construct() {
+
+  }
+
+  public function add($a, $b) {
+    return $a + $b;
+  }
+}
